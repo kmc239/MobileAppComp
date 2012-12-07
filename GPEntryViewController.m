ead9acf4a9c94e4ab89ac05ac4fba1921c05e578
kmc239 <kyle.clegg@gmail.com>
2012-12-06 22:44:08 -0700

    Updated EntryViewController to use RestKit to download image asynchronously.

--- GPEntryViewController.m
+++ GPEntryViewController.m
@@ -9,6 +9,7 @@
 #import "GPEntryViewController.h"
 #import "GPHelpers.h"
 #import <QuartzCore/QuartzCore.h>
+#import <RestKit/RestKit.h>
 
 @interface GPEntryViewController ()
 
@@ -36,19 +37,26 @@
   // Set custom font for title
   [GPHelpers setCustomFontsForTitle:NSLocalizedString(@"ENTRY", nil) forViewController:self];
   
-  // Load the picture
-//  UIImageView *pictureImageView = (UIImageView *)[cell viewWithTag:1];
+  // Use RestKit to download image asychronously
   GPPicture *picture = self.currentEntry.picture;
   NSString *baseUrl = [[RKClient sharedClient] baseURL].absoluteString;
   NSURL *imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", baseUrl, picture.pictureUrl]];
-  NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
-  UIImage *image = [UIImage imageWithData:imageData];
-  self.entryPicture.image = image;
+  RKRequest* request = [RKRequest requestWithURL: imageURL];
   
-  if (picture.pictureUrl == nil) {
-    UIImage *stockImage = [UIImage imageNamed:@"TakePhoto.png"];
-    self.entryPicture.image = stockImage;
-  }
+  request.onDidLoadResponse = ^(RKResponse* response) {
+    UIImage* image = [UIImage imageWithData: response.body];
+    self.entryPicture.image = image;
+    DLog(@"image success");
+  };
+  request.onDidFailLoadWithError = ^(NSError* error) {
+    // handle failure to load image
+    DLog(@"image error");
+  };
+  
+  RKRequestQueue *imageLoadingQueue = [RKRequestQueue requestQueueWithName: @"imageLoadingQueue"];
+  [imageLoadingQueue start];
+  
+  [imageLoadingQueue addRequest: request];
   
   // Make picture circular
   self.entryPicture.layer.cornerRadius = 5.0;

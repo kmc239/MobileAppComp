cae17b20ff077b8b16ffa759a72119b91e10b83f
kmc239 <kyle.clegg@gmail.com>
2013-01-20 23:52:33 -0700

    Added Twitter functionality to share individual pictures.

--- GPEntryViewController.m
+++ GPEntryViewController.m
@@ -10,6 +10,7 @@
 #import "GPHelpers.h"
 #import <QuartzCore/QuartzCore.h>
 #import <RestKit/RestKit.h>
+#import "Twitter/TWTweetComposeViewController.h"
 
 @interface GPEntryViewController ()
 
@@ -20,15 +21,6 @@
 @synthesize currentEntry = _currentEntry;
 @synthesize entryPicture = _entryPicture;
 @synthesize descriptionLabel = _descriptionLabel;
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-  if (self) {
-    // Custom initialization
-  }
-  return self;
-}
 
 - (void)viewDidLoad
 {
@@ -50,15 +42,46 @@
   self.entryPicture.layer.masksToBounds = YES;
 }
 
-- (void)didReceiveMemoryWarning
-{
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
-
 - (IBAction)twitterShare:(id)sender
 {
-
+  Class tweeterClass = NSClassFromString(@"TWTweetComposeViewController");
+  
+  if(tweeterClass != nil) {   // check for Twitter integration
+    
+    // check Twitter accessibility and at least one account is setup
+    if([TWTweetComposeViewController canSendTweet]) {
+      
+      UIGraphicsBeginImageContextWithOptions(self.entryPicture.bounds.size, NO, 0.0);
+      [self.entryPicture.image drawInRect:CGRectMake(0, 0, self.entryPicture.frame.size.width, self.entryPicture.frame.size.height)];
+      UIImage *SaveImage = UIGraphicsGetImageFromCurrentImageContext();
+      UIGraphicsEndImageContext();
+      
+      TWTweetComposeViewController *tweetViewController = [[TWTweetComposeViewController alloc] init];
+      [tweetViewController setInitialText:[NSString stringWithFormat:@"%@ - day 16 via @growingpainsapp", self.currentEntry.description]];
+      
+      // add image
+      [tweetViewController addImage:SaveImage];
+      tweetViewController.completionHandler = ^(TWTweetComposeViewControllerResult result) {
+        
+        if(result == TWTweetComposeViewControllerResultDone) {
+          DLog(@"Tweet Sent");
+        } else if(result == TWTweetComposeViewControllerResultCancelled) {
+          DLog(@"Tweet Cancelled");
+        }
+        [self dismissViewControllerAnimated:YES completion:nil];
+      };
+      
+      [self presentViewController:tweetViewController animated:YES completion:nil];
+      
+    }
+    else {
+      UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry" message:@"You can't send a tweet right now, make sure you have at least one Twitter account setup" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+      [alertView show];
+    }
+  } else {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry" message:@"You must upgrade to iOS 5 in order to send tweets from Growing Pains" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alertView show];
+  }
 }
 
 - (IBAction)facebookShare:(id)sender
@@ -70,4 +93,5 @@
 {
   
 }
+
 @end
